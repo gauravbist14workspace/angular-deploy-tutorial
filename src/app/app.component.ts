e45fa163f6015a13674e58443efad5a9df9c6c23
Gaurav Bisht <gauravbist14workspace@gmail.com>
2018-11-23 00:09:01 +0530

    Dynamic carousel using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-new-proj';
+  
+  selectedImageIndex: number;
+  imgs: any[];
+
+  constructor() {
+    this.imgs = [
+      "https://www.w3schools.com/bootstrap/la.jpg",
+      "https://www.w3schools.com/bootstrap/chicago.jpg",
+      "https://www.w3schools.com/bootstrap/ny.jpg"
+    ]
+
+    this.selectedImageIndex = 0;
+  }
+
+  nextPic() {
+    if (this.selectedImageIndex == this.imgs.length - 1) {
+      this.selectedImageIndex = 0;
+    } else {
+      this.selectedImageIndex++;
+    }
+  }
+
+  prevPic() {
+    if (this.selectedImageIndex == 0) {
+      this.selectedImageIndex = this.imgs.length - 1;
+    } else {
+      this.selectedImageIndex--;
+    }
+  }
 }
